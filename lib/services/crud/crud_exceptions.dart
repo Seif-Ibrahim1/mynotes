5c25dc4be4be370628c8092ed956e610b69fc9dc
Seif-Ibrahim1 <se.seifibrahim@gmail.com>
2022-08-04 19:50:28 +0200

    Step 18

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -15,3 +15,7 @@ class CouldNotDeleteNote implements Exception {}
 class CouldNotFindNote implements Exception {}
 
 class CouldNotUpdateNote implements Exception {}
+
+class UserShouldBeSetBeforeReadingAllNotes implements Exception {}
+
+
