9844181befc174cdb8a6c2ba634f6602aae52242
Seif-Ibrahim1 <se.seifibrahim@gmail.com>
2022-08-05 20:41:19 +0200

    Step 20

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -39,27 +39,26 @@ class FirebaseCloudStorage {
           )
           .get()
           .then(
-            (value) => value.docs.map(
-              (doc) {
-                return CloudNote(
-                  documentId: doc.id,
-                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
-                  text: doc.data()[textFieldName] as String,
-                );
-              },
-            ),
+            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
           );
     } catch (e) {
       throw CouldNotGetAllNotesException();
     }
   }
 
-  void createNote({required String ownerUserId}) async {
-    await notes.add(
+  Future<CloudNote> createNewNote({required String ownerUserId}) async {
+    final document = await notes.add(
       {
         ownerUserIdFieldName: ownerUserId,
         textFieldName: '',
       },
+    );
+
+    final fetchedNote = await document.get();
+    return CloudNote(
+      documentId: fetchedNote.id,
+      ownerUserId: ownerUserId,
+      text: '',
     );
   }
 
