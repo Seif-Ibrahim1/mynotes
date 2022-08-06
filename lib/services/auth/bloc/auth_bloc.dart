06c6bea5200e74604521d53a2f6a7358b14efedf
Seif-Ibrahim1 <se.seifibrahim@gmail.com>
2022-08-06 19:36:18 +0200

    Step 23

---
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    //initialize
+    on<AuthEventInitialize>((event, emit) async {
+      await provider.initialize();
+      final user = provider.currentUser;
+      if (user == null) {
+        emit(const AuthStateLoggedOut());
+      } else if (!user.isEmailVerified) {
+        emit(const AuthStateNeedsVerification());
+      } else {
+        emit(AuthStateLoggedIn(user));
+      }
+    });
+
+    //login
+    on<AuthEventLogin>((event, emit) async {
+      emit(const AuthStateLoading());
+      final email = event.email;
+      final password = event.password;
+      try {
+        final user = await provider.login(
+          email: email,
+          password: password,
+        );
+        emit(AuthStateLoggedIn(user));
+      } on Exception catch (e) {
+        emit(AuthStateLoginFailure(e));
+      }
+    });
+
+    //logout
+    on<AuthEventLogout>((event, emit) async {
+      try {
+        emit(const AuthStateLoading());
+        await provider.logout();
+        emit(const AuthStateLoggedOut());
+      } on Exception catch (e) {
+        emit(AuthStateLogoutFailure(e));
+      }
+    });
+  }
+}
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -61,26 +64,12 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await AuthService.firebase().login(
-                  email: email,
-                  password: password,
-                );
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  notesRoute,
-                  (route) => false,
-                );
-                final user = AuthService.firebase().currentUser;
-                if (user?.isEmailVerified ?? false) {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
-                } else {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    verifyEmailRoute,
-                    (route) => false,
-                  );
-                }
+                context.read<AuthBloc>().add(
+                      AuthEventLogin(
+                        email,
+                        password,
+                      ),
+                    );
               } on UserNotFoundAuthException {
                 await showErrorDialog(context, "User not found");
               } on WrongPasswordAuthException {
